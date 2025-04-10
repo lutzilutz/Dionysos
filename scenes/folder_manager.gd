803431bc6033627e1b68a54b6cb804f263b65c61
Lutzi Lutz <lutzilutz11@gmail.com>
2025-04-10 18:33:07 +0200

    Corrected form behavior with alphanumerics and emphasis

--- scenes/folder_manager.gd
+++ scenes/folder_manager.gd
@@ -360,8 +360,8 @@ func check_tree_item_for_audio_sfx(tree_item: TreeItem) -> void:
 func update_controls() -> void: # Updating form controls depending how much user has filled it
 	
 	var customer_editable = main_scene.user_preferences.has_default_path and not info_locked
-	var project_name_editable = customer_editable and customer_name != "" and is_customer_name_valid(customer_name)
-	var production_type_editable = project_name_editable and project_name != "" and is_project_name_valid(project_name)
+	var project_name_editable = customer_editable and customer_name != ""# and is_customer_name_valid(customer_name)
+	var production_type_editable = project_name_editable and project_name != ""# and is_project_name_valid(project_name)
 	var editor_editable = production_type_editable and production_type_option.selected != -1
 	var secondary_options_editable = editor_editable and editor_option.selected != -1
 	
@@ -381,42 +381,50 @@ func update_controls() -> void: # Updating form controls depending how much user
 	customer_option.toggle_emphasis(customer_editable and not project_name_editable)
 	customer_edit.editable = customer_editable and customer_option.selected == 0
 	customer_edit.toggle_emphasis(customer_editable and not project_name_editable)
+	
 	if customer_editable and customer_name != "" and is_customer_name_valid(customer_name):
 		customer_edit.modulate = Color(1,1,1,1)
 		customer_line.get_node("LineIcon").texture = main_scene.checked_texture
 		customer_line.get_node("LineIcon").modulate = main_scene.checked_color
 		customer_line.get_node("LineIcon").tooltip_text = ""
 	else:
-		customer_edit.modulate = Color(1,0.5,0,1)
-		customer_line.get_node("LineIcon").texture = main_scene.error_texture
-		customer_line.get_node("LineIcon").modulate = main_scene.warning_color
-		customer_line.get_node("LineIcon").tooltip_text = "Ce nom ne peut pas être vide et ne doit pas contenir de caractères spéciaux !"
-	customer_line.get_node("LineIcon").texture = main_scene.checked_texture if project_name_editable else main_scene.empty_texture
-	customer_line.get_node("LineIcon").modulate = main_scene.checked_color
+		if not is_customer_name_valid(customer_name):
+			customer_edit.modulate = Color(1,0.5,0,1)
+			customer_line.get_node("LineIcon").texture = main_scene.error_texture
+			customer_line.get_node("LineIcon").modulate = main_scene.warning_color
+			customer_line.get_node("LineIcon").tooltip_text = "Ce nom ne doit pas contenir de caractères spéciaux !"
+		else:
+			customer_line.get_node("LineIcon").texture = main_scene.checked_texture if project_name_editable else main_scene.empty_texture
+			customer_line.get_node("LineIcon").modulate = main_scene.checked_color
+			customer_line.get_node("LineIcon").tooltip_text = ""
 	
 	# Project name
 	project_name_label.disable(not project_name_editable)
 	project_name_edit.editable = project_name_editable
 	project_name_edit.toggle_emphasis(project_name_editable and not production_type_editable)
-	if project_name_editable and project_name != "":
+	if project_name_editable and project_name != "" and is_project_name_valid(project_name):
 		if path_has_conflict():
 			project_name_edit.modulate = Color(1,0.5,0,1)
 			project_name_line.get_node("LineIcon").texture = main_scene.error_texture
 			project_name_line.get_node("LineIcon").modulate = main_scene.warning_color
 			project_name_line.get_node("LineIcon").tooltip_text = "Un projet avec le même nom existe déjà pour ce client !"
 		else:
-			if is_project_name_valid(project_name) :
-				project_name_edit.modulate = Color(1,1,1,1)
-				project_name_line.get_node("LineIcon").texture = main_scene.checked_texture
-				project_name_line.get_node("LineIcon").modulate = main_scene.checked_color
-				project_name_line.get_node("LineIcon").tooltip_text = ""
-			else:
-				project_name_edit.modulate = Color(1,0.5,0,1)
-				project_name_line.get_node("LineIcon").texture = main_scene.error_texture
-				project_name_line.get_node("LineIcon").modulate = main_scene.warning_color
-				project_name_line.get_node("LineIcon").tooltip_text = "Ce nom ne peut pas être vide et ne doit pas contenir de caractères spéciaux !"
-	if not production_type_editable:
-		project_name_line.get_node("LineIcon").texture = main_scene.empty_texture
+			project_name_edit.modulate = Color(1,1,1,1)
+			project_name_line.get_node("LineIcon").texture = main_scene.checked_texture
+			project_name_line.get_node("LineIcon").modulate = main_scene.checked_color
+			project_name_line.get_node("LineIcon").tooltip_text = ""
+	else:
+		if not is_project_name_valid(project_name):
+			project_name_edit.modulate = Color(1,0.5,0,1)
+			project_name_line.get_node("LineIcon").texture = main_scene.error_texture
+			project_name_line.get_node("LineIcon").modulate = main_scene.warning_color
+			project_name_line.get_node("LineIcon").tooltip_text = "Ce nom ne doit pas contenir de caractères spéciaux !"
+		else:
+			project_name_line.get_node("LineIcon").texture = main_scene.empty_texture
+			project_name_line.get_node("LineIcon").modulate = main_scene.checked_color
+			project_name_line.get_node("LineIcon").tooltip_text = ""
+	#if not production_type_editable:
+		#project_name_line.get_node("LineIcon").texture = main_scene.empty_texture
 	
 	# Production type
 	production_type_label.disable(not production_type_editable)
@@ -459,8 +467,15 @@ func update_controls() -> void: # Updating form controls depending how much user
 	vfx_checkbox.disabled = not secondary_options_editable
 	
 	# Buttons
-	preview_folder_button.disabled = not (main_scene.user_preferences.has_default_path and customer_name != "" and project_name != "" and production_type_option.selected != -1 and editor_name != "" and not path_has_conflict())
-	generate_folder_button.disabled = not (main_scene.user_preferences.has_default_path and customer_name != "" and project_name != "" and production_type_option.selected != -1 and editor_name != "" and info_locked)
+	#preview_folder_button.disabled = not (main_scene.user_preferences.has_default_path and customer_name != "" and project_name != "" and production_type_option.selected != -1 and editor_name != "" and not path_has_conflict())
+	preview_folder_button.disabled = not can_preview()
+	preview_folder_button.toggle_emphasis(secondary_options_editable)
+	if can_preview():
+		if can_generate():
+			preview_folder_button.button_text = "Annuler"
+		else:
+			preview_folder_button.button_text = "Verrouiller et prévisualiser"
+	generate_folder_button.disabled = not can_generate()
 	
 	# Tree
 	if customer_name != "" and project_name != "":
@@ -471,6 +486,17 @@ func update_controls() -> void: # Updating form controls depending how much user
 		folder_tree.clear()
 		summary_title.visible = false
 		summary_body.visible = false
+
+func can_preview() -> bool:
+	var is_customer_ok: bool = customer_name != "" and is_customer_name_valid(customer_name)
+	var is_project_ok: bool = project_name != "" and is_project_name_valid(project_name) and not path_has_conflict()
+	var is_production_type_ok: bool = production_type_option.selected != -1
+	var is_editor_ok: bool = editor_name != ""
+	return main_scene.user_preferences.has_default_path	and is_customer_ok and is_project_ok and is_production_type_ok and is_editor_ok
+	
+
+func can_generate() -> bool:
+	return can_preview() and info_locked
 
 func path_has_conflict() -> bool:
 	return DirAccess.dir_exists_absolute(main_scene.user_preferences.default_path + "/" + customer_name + "/" + project_name)
--- scenes/forms/form_button.gd
+++ scenes/forms/form_button.gd
@@ -2,6 +2,7 @@
 extends Button
 
 @export var button_text: String = "": set = set_button_text
+#@export var button_text_alt: String = "": set = set_button_text_alt
 var is_emphasized: bool = false
 
 func toggle_emphasis(value: bool) -> void:
@@ -11,3 +12,7 @@ func toggle_emphasis(value: bool) -> void:
 func set_button_text(value: String) -> void:
 	button_text = value
 	get_node("MarginContainer/Label").text = value
+
+#func set_button_text_alt(value: String) -> void:
+	#button_text = value
+	#get_node("MarginContainer/Label").text = value
