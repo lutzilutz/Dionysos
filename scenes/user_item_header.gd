5043691b8703f563760e50efd38423e813207f2b
Lutzi Lutz <lutzilutz11@gmail.com>
2025-04-14 15:01:38 +0200

    Added ascension indication in UserManager

--- scenes/user_item_header.gd
+++ scenes/user_item_header.gd
@@ -16,26 +16,53 @@ func update_flags(type: DataManager.SortType) -> void:
 	match type:
 		DataManager.SortType.NAME:
 			phone_ascending = true
+			reset_label(DataManager.SortType.PHONE)
 			email_ascending = true
+			reset_label(DataManager.SortType.EMAIL)
 			function_ascending = true
+			reset_label(DataManager.SortType.FUNCTION)
 		DataManager.SortType.PHONE:
 			name_ascending = true
+			reset_label(DataManager.SortType.NAME)
 			email_ascending = true
+			reset_label(DataManager.SortType.EMAIL)
 			function_ascending = true
+			reset_label(DataManager.SortType.FUNCTION)
 		DataManager.SortType.EMAIL:
 			name_ascending = true
+			reset_label(DataManager.SortType.NAME)
 			phone_ascending = true
+			reset_label(DataManager.SortType.PHONE)
 			function_ascending = true
+			reset_label(DataManager.SortType.FUNCTION)
 		DataManager.SortType.FUNCTION:
 			name_ascending = true
+			reset_label(DataManager.SortType.NAME)
 			phone_ascending = true
-			function_ascending = true
+			reset_label(DataManager.SortType.PHONE)
+			email_ascending = true
+			reset_label(DataManager.SortType.EMAIL)
+
+func reset_label(type: DataManager.SortType) -> void:
+	if type == DataManager.SortType.NAME:
+		get_node("MarginContainer/UserContainer/UserNameButton/UserNameLabel").text = "Nom de l'utilisateur"
+	elif type == DataManager.SortType.PHONE:
+		get_node("MarginContainer/UserContainer/UserPhoneButton/UserPhoneLabel").text = "Téléphone"
+	elif type == DataManager.SortType.EMAIL:
+		get_node("MarginContainer/UserContainer/UserEmailButton/UserEmailLabel").text = "E-mail"
+	elif type == DataManager.SortType.FUNCTION:
+		get_node("MarginContainer/UserContainer/UserFunctionButton/UserFunctionLabel").text = "Fonction"
 
 func _on_user_name_button_pressed() -> void:
 	if currently_sorted_by == DataManager.SortType.NAME:
 		name_ascending = not name_ascending
 	else:
 		update_flags(DataManager.SortType.NAME)
+	
+	if name_ascending:
+		get_node("MarginContainer/UserContainer/UserNameButton/UserNameLabel").text = "Nom de l'utilisateur ˅"
+	else:
+		get_node("MarginContainer/UserContainer/UserNameButton/UserNameLabel").text = "Nom de l'utilisateur ˄"
 	currently_sorted_by = DataManager.SortType.NAME
 	sort_by_name.emit(name_ascending)
 
@@ -44,6 +71,10 @@ func _on_user_phone_button_pressed() -> void:
 		phone_ascending = not phone_ascending
 	else:
 		update_flags(DataManager.SortType.PHONE)
+	if phone_ascending:
+		get_node("MarginContainer/UserContainer/UserPhoneButton/UserPhoneLabel").text = "Téléphone ˅"
+	else:
+		get_node("MarginContainer/UserContainer/UserPhoneButton/UserPhoneLabel").text = "Téléphone ˄"
 	currently_sorted_by = DataManager.SortType.PHONE
 	sort_by_phone.emit(phone_ascending)
 
@@ -52,6 +83,10 @@ func _on_user_email_button_pressed() -> void:
 		email_ascending = not email_ascending
 	else:
 		update_flags(DataManager.SortType.EMAIL)
+	if email_ascending:
+		get_node("MarginContainer/UserContainer/UserEmailButton/UserEmailLabel").text = "E-mail ˅"
+	else:
+		get_node("MarginContainer/UserContainer/UserEmailButton/UserEmailLabel").text = "E-mail ˄"
 	currently_sorted_by = DataManager.SortType.EMAIL
 	sort_by_email.emit(email_ascending)
 
@@ -60,5 +95,9 @@ func _on_user_function_button_pressed() -> void:
 		function_ascending = not function_ascending
 	else:
 		update_flags(DataManager.SortType.FUNCTION)
+	if function_ascending:
+		get_node("MarginContainer/UserContainer/UserFunctionButton/UserFunctionLabel").text = "Fonction ˅"
+	else:
+		get_node("MarginContainer/UserContainer/UserFunctionButton/UserFunctionLabel").text = "Fonction ˄"
 	currently_sorted_by = DataManager.SortType.FUNCTION
 	sort_by_function.emit(function_ascending)
