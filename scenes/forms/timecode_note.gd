d07713e558f56188c535fa6b6a52ec4a6a124b29
Lutzi Lutz <lutzilutz11@gmail.com>
2025-04-23 18:52:16 +0200

    Added working timecode fields (no file generation yet)

--- scenes/forms/timecode_note.gd
+++ scenes/forms/timecode_note.gd
@@ -1,10 +1,46 @@
 extends HBoxContainer
 
-signal text_submitted_or_next
+signal text_submitted_or_next(line_id)
+
+var line_id: int = 0
+
+func set_line_id(new_id: int) -> void:
+	line_id = new_id
+
+func get_hour() -> int:
+	if get_node("HourEdit").text != "":
+		return int(get_node("HourEdit").text)
+	else:
+		return 0
+
+func get_minute() -> int:
+	if get_node("MinuteEdit").text != "":
+		return int(get_node("MinuteEdit").text)
+	else:
+		return 0
+
+func get_second() -> int:
+	if get_node("SecondEdit").text != "":
+		return int(get_node("SecondEdit").text)
+	else:
+		return 0
+
+func get_frame() -> int:
+	if get_node("FrameEdit").text != "":
+		return int(get_node("FrameEdit").text)
+	else:
+		return 0
+
+func has_timecode() -> bool:
+	if get_node("HourEdit").text != "" or get_node("MinuteEdit").text != "" or get_node("SecondEdit").text != "" or get_node("FrameEdit").text != "":
+		return true
+	return false
+
+# Signals -----------------------------------------------------------------------------------------
 
 func _on_text_edit_text_submitted(new_text: String) -> void:
-	text_submitted_or_next.emit()
+	text_submitted_or_next.emit(line_id)
 
 func _on_text_edit_focus_exited() -> void:
 	if get_node("TextEdit").text != "":
-		text_submitted_or_next.emit()
+		text_submitted_or_next.emit(line_id)
--- timecode_notes.gd
+++ timecode_notes.gd
@@ -4,11 +4,59 @@ const timecode_note_scene: PackedScene = preload("res://scenes/forms/timecode_no
 
 func _ready() -> void:
 	get_node("TimecodeNote").text_submitted_or_next.connect(_on_text_submitted_or_next)
+	get_node("TimecodeNote").set_line_id(0)
 
-func _on_text_submitted_or_next() -> void:
-	var new_note = timecode_note_scene.instantiate()
-	add_child(new_note)
-	new_note.text_submitted_or_next.connect(_on_text_submitted_or_next)
-	#new_note.get_node("TextEdit").grab_focus()
-	print("new line")
-	pass
+func sort_notes_by_time() -> void:
+	var children_notes := get_children()
+	children_notes.sort_custom(compare_times)
+	for c in get_children():
+		remove_child(c)
+	for c in children_notes:
+		add_child(c)
+
+func compare_times(a, b) -> bool:
+	# Exception for a newly created line (which is empty for now)
+	if a.line_id == get_child_count()-1:
+		return false
+	
+	# Both lines have some timecode
+	if a.has_timecode and b.has_timecode:
+		if a.get_hour() < b.get_hour():
+			return true
+		elif a.get_hour() == b.get_hour():
+			if a.get_minute() < b.get_minute():
+				return true
+			elif a.get_minute() == b.get_minute():
+				if a.get_second() < b.get_second():
+					return true
+				elif a.get_second() == b.get_second():
+					if a.get_frame() < b.get_frame():
+						return true
+					elif a.get_frame() == b.get_frame():
+						return a.line_id < b.line_id
+	elif not a.has_timecode and b.has_timecode:
+		return true
+	elif a.has_timecode and not b.has_timecode:
+		return false
+	elif not a.has_timecode and not b.has_timecode:
+		return a.line_id < b.line_id
+	
+	return false
+
+# Signals -----------------------------------------------------------------------------------------
+
+func _on_text_submitted_or_next(line_id: int) -> void:
+	if line_id == get_child_count()-1:
+		var new_note = timecode_note_scene.instantiate()
+		add_child(new_note)
+		new_note.text_submitted_or_next.connect(_on_text_submitted_or_next)
+		new_note.set_line_id(get_child_count()-1)
+		
+		# Go to previous control (coz of automatic tab behavior)
+		var event := InputEventKey.new()
+		event.pressed = true
+		event.keycode = KEY_TAB
+		event.shift_pressed = true
+		Input.parse_input_event(event)
+		
+		sort_notes_by_time()
